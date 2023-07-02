20bb34e51eb5a2f6c08300dc6511b203dd38076f
Russell Larkin <russell.larkin@gmail.com>
2023-07-03 00:29:01 +0100

    Foundry VTT V11, Refactor, Restyle Dice Tray - Update Foundry VTT verified to 11.304. - Refactor code. - Restyle elements of dice tray module.

--- scripts/combat-chat.js
+++ scripts/combat-chat.js
@@ -1,209 +1,177 @@
-const MODULE_NAME = 'combat-chat'
-const DISPLAY_NAME = 'Combat Chat'
-const CHAT_ON_COMBAT_TRACKER_SETTING = 'showChatOnCombatTrackerTab'
-const CHAT_INPUT_IN_SPLIT_VIEW = 'showChatInputInSplitView'
-const CHAT_HEIGHT_SETTING = 'chatHeight'
-const COMBAT_CHAT_CLASS = 'combat-chat'
-const HIDE_CHAT_INPUT_CLASS = 'hide-chat-input'
-const CHAT_ID = 'chat'
-const COMBAT_ID = 'combat'
-const ACTIVE_CLASS = 'active'
-const STYLE_ID = `${MODULE_NAME}-styles`
-const DRAG_AREA_HEIGHT = 8
+import {
+    SETTING,
+    COMBAT_CHAT_CLASS,
+    COMBAT_CHAT_DICE_TRAY_CLASS,
+    HIDE_CHAT_INPUT_CLASS,
+    CHAT_ID,
+    CHAT_LOG_ID,
+    COMBAT_ID,
+    TABS_ID,
+    ACTIVE_CLASS,
+    STYLE_ID
+} from './constants.js'
+import { registerSettings } from './settings.js'
+import { Utils } from './utils.js'
 
-function isChatOnCombatTrackerEnabled () {
-  return game.settings.get(MODULE_NAME, CHAT_ON_COMBAT_TRACKER_SETTING)
-}
+export class CombatChat {
+    constructor () {
+        this.resizing = false
+        this.dragStartRegistered = false
+    }
 
-function isChatInputOnCombatTrackerEnabled () {
-  return game.settings.get(MODULE_NAME, CHAT_INPUT_IN_SPLIT_VIEW)
+    init () {
+        registerSettings()
+        this.getSettings()
+        this.createStyleElement()
+    }
+
+    ready () {
+        this.getElements()
+        this.addDividerElement()
+        this.injectSidebarHook()
+        this.registerDragResizeListeners()
+        this.updateCombatTrackerStyle()
+        this.updateStyleElement()
+    }
+
+    getElements () {
+        this.chatElement = Utils.getElementById(CHAT_ID)
+        this.chatLogElement = Utils.getElementById(CHAT_LOG_ID)
+        this.diceTrayElement = Utils.getElementBySelector(`#${CHAT_ID} .dice-tray`)
+        this.tabsElement = Utils.getElementById(TABS_ID)
+    }
+
+    addDividerElement () {
+        this.dividerElement = document.createElement('div')
+        this.dividerElement.setAttribute('id', 'combat-chat-divider')
+        this.dividerElement.innerHTML = '<div></div>'
+        this.dividerElement.addEventListener('pointerdown', () => { this.onStartDrag() })
+        this.chatElement.insertBefore(this.dividerElement, this.chatElement.firstChild)
+    }
+
+    createStyleElement () {
+        const style = document.createElement('style')
+        style.setAttribute('id', STYLE_ID)
+        document.head.append(style)
+        return style
+    }
+
+    isScrolledToBottom () {
+        if (this.chatLogElement != null) {
+            return this.chatLogElement.scrollHeight - this.chatLogElement.offsetHeight === this.chatLogElement.scrollTop
+        } else {
+            return false
+        }
+    }
+
+    scrollChatToEnd () {
+        if (this.chatLogElement != null && typeof this.chatLogElement.scrollTo === 'function') {
+            this.chatLogElement.scrollTo({ top: this.chatLogElement.scrollHeight - this.chatLogElement.offsetHeight })
+        }
+    }
+
+    updateStyleElement () {
+        const tabsHeight = this.tabsElement.offsetHeight
+        const chatHeight = Utils.getSetting(SETTING.CHAT_HEIGHT)
+        const sidebarHeight = ui.sidebar.element.height()
+        const maxHeight = sidebarHeight - tabsHeight - 2
+        if (chatHeight != null && chatHeight !== 0) {
+            const scrolledToBottom = this.isScrolledToBottom()
+            const style = document.getElementById(STYLE_ID)
+            if (style !== null) {
+                style.innerHTML = `
+                    #${CHAT_ID}.${COMBAT_CHAT_CLASS} {
+                    flex-grow: 0 !important;
+                    flex-shrink: 0 !important;
+                    flex-basis: ${chatHeight}px !important;
+                    max-height: ${maxHeight}px !important;
+                    }
+                `
+                if (scrolledToBottom) {
+                    requestAnimationFrame(() => this.scrollChatToEnd())
+                }
+            }
+        }
+    }
+
+    getSettings () {
+        this.isDisplayChatLog = Utils.getSetting(SETTING.CHAT_LOG_DISPLAY)
+        this.isDisplayChatInput = Utils.getSetting(SETTING.CHAT_INPUT_DISPLAY)
+        this.isRestyleDiceTray = Utils.getSetting(SETTING.RESTYLE_DICE_TRAY)
+    }
+
+    updateSettings () {
+        this.getSettings()
+        this.updateCombatTrackerStyle()
+    }
+
+    updateCombatTrackerStyle () {
+        const activeTab = Utils.getActiveTab()
+
+        if (this.isDisplayChatLog && activeTab === COMBAT_ID) {
+            this.chatElement.classList.add(ACTIVE_CLASS, COMBAT_CHAT_CLASS)
+            this.diceTrayElement?.classList?.toggle(COMBAT_CHAT_DICE_TRAY_CLASS, this.isRestyleDiceTray)
+            this.scrollChatToEnd()
+        } else {
+            if (activeTab !== CHAT_ID) this.chatElement.classList.remove(ACTIVE_CLASS)
+            this.chatElement.classList.remove(COMBAT_CHAT_CLASS)
+            this.diceTrayElement?.classList?.remove(COMBAT_CHAT_DICE_TRAY_CLASS)
+        }
+
+        this.chatElement.classList.toggle(HIDE_CHAT_INPUT_CLASS, this.isDisplayChatLog && !this.isDisplayChatInput && activeTab === COMBAT_ID)
+    }
+
+    injectSidebarHook () {
+        const sidebar = ui.sidebar._tabs[0]
+        const originalMethod = sidebar.activate
+        sidebar.activate = function activateOverride (name, ...rest) {
+            originalMethod.call(this, name, ...rest)
+            game.combatChat.updateCombatTrackerStyle()
+        }
+    }
+
+    isInDragArea (event) {
+        return event.target === this.dividerElement
+    }
+
+    onStartDrag () {
+        this.resizing = true
+    }
+
+    onDrag (event) {
+        if (this.resizing) {
+            const newHeight = this.chatElement.offsetHeight - event.movementY
+            Utils.setSetting(SETTING.CHAT_HEIGHT, newHeight)
+            this.updateStyleElement()
+        }
+    }
+
+    onStopDrag () {
+        if (this.resizing) {
+            this.resizing = false
+        }
+    }
+
+    /**
+ * LISTENERS
+ */
+
+    registerDragResizeListeners () {
+        document.addEventListener('pointermove', (event) => { this.onDrag(event) })
+        document.addEventListener('pointerup', (event) => { this.onStopDrag(event) })
+        document.addEventListener('pointercancel', (event) => { this.onStopDrag(event) })
+    }
 }
 
 /**
- * @returns {null|string}
+ * HOOKS
  */
-function getActiveTab () {
-  if (ui.sidebar != null) {
-    return ui.sidebar.activeTab
-  } else {
-    return null
-  }
-}
-
-function getChatElement () {
-  return document.getElementById(CHAT_ID)
-}
-
-function getChatLog () {
-  return document.getElementById('chat-log')
-}
-
-function isScrolledToBottom () {
-  const chatLog = getChatLog()
-  if (chatLog != null) {
-    return chatLog.scrollHeight - chatLog.offsetHeight === chatLog.scrollTop
-  } else {
-    return false
-  }
-}
-
-function scrollChatToEnd () {
-  const chatLog = getChatLog()
-  if (chatLog != null && typeof chatLog.scrollTo === 'function') {
-    chatLog.scrollTo({ top: chatLog.scrollHeight - chatLog.offsetHeight })
-  }
-}
-
-function createStyleElement () {
-  const style = document.createElement('style')
-  style.setAttribute('id', STYLE_ID)
-  document.head.append(style)
-  return style
-}
-
-function updateStyleElement () {
-  const chatHeight = game.settings.get(MODULE_NAME, CHAT_HEIGHT_SETTING)
-  if (chatHeight != null && chatHeight !== 0) {
-    const scrolledToBottom = isScrolledToBottom()
-    const style = document.getElementById(STYLE_ID)
-    if (style != null) {
-      style.innerText = `
-        #${CHAT_ID}.${COMBAT_CHAT_CLASS} {
-          flex-grow: 0 !important;
-          flex-shrink: 0 !important;
-          flex-basis: ${chatHeight}px !important;
-        }
-      `
-      if (scrolledToBottom) {
-        requestAnimationFrame(() => scrollChatToEnd())
-      }
-    }
-  }
-}
-
-function updateCombatTrackerStyle () {
-  const enable = isChatOnCombatTrackerEnabled()
-  const activeTab = getActiveTab()
-  const chatElement = getChatElement()
-  if (enable && activeTab === COMBAT_ID) {
-    chatElement.classList.add(ACTIVE_CLASS, COMBAT_CHAT_CLASS)
-    scrollChatToEnd()
-  } else if (activeTab !== CHAT_ID) {
-    chatElement.classList.remove(ACTIVE_CLASS)
-  } else if (activeTab !== COMBAT_ID) {
-    chatElement.classList.remove(COMBAT_CHAT_CLASS)
-  }
-
-  if (!isChatInputOnCombatTrackerEnabled() && activeTab === COMBAT_ID) {
-    chatElement.classList.add(HIDE_CHAT_INPUT_CLASS)
-  } else {
-    chatElement.classList.remove(HIDE_CHAT_INPUT_CLASS)
-  }
-}
-
-function registerSettings () {
-  game.settings.register(MODULE_NAME, CHAT_ON_COMBAT_TRACKER_SETTING, {
-    name: game.i18n?.localize('combatChat.settings.showChatOnCombatTrackerTab.name'),
-    hint: game.i18n?.localize('combatChat.settings.showChatOnCombatTrackerTab.hint'),
-    scope: 'client',
-    config: true,
-    type: Boolean,
-    default: true,
-    onChange: updateCombatTrackerStyle
-  })
-
-  game.settings.register(MODULE_NAME, CHAT_INPUT_IN_SPLIT_VIEW, {
-    name: game.i18n?.localize('combatChat.settings.showChatInputInSplitView.name'),
-    hint: game.i18n?.localize('combatChat.settings.showChatInputInSplitView.hint'),
-    scope: 'client',
-    config: true,
-    type: Boolean,
-    default: true,
-    onChange: updateCombatTrackerStyle
-  })
-
-  game.settings.register(MODULE_NAME, CHAT_HEIGHT_SETTING, {
-    name: 'Chat height',
-    hint: 'Changes the chat window height when displaying it below the combat tracker',
-    scope: 'client',
-    config: false, // < Non-UI setting. Adjusted via drag and drop.
-    type: Number,
-    default: null
-  })
-}
-
-function injectSidebarHook () {
-  const sidebar = ui.sidebar._tabs[0]
-  const originalMethod = sidebar.activate
-  sidebar.activate = function activateOverride (name, ...rest) {
-    originalMethod.call(this, name, ...rest)
-    updateCombatTrackerStyle()
-  }
-}
-
-function isInDragArea (chatElement, event) {
-  return event.target === chatElement && event.offsetY < DRAG_AREA_HEIGHT
-}
-
-let resizing = false
-
-function onStartDrag (chatElement, event) {
-  if (isInDragArea(chatElement, event)) {
-    resizing = true
-  }
-}
-
-function onDrag (event) {
-  if (resizing) {
-    const chatElement = getChatElement()
-    const newHeight = chatElement.offsetHeight - event.movementY
-    game.settings.set(MODULE_NAME, CHAT_HEIGHT_SETTING, newHeight)
-    updateStyleElement()
-  }
-}
-
-function onStopDrag () {
-  if (resizing) {
-    resizing = false
-  }
-}
-
-let dragStartRegistered = false
-
-function registerDragStartListener () {
-  if (dragStartRegistered) {
-    return
-  }
-
-  const chatElement = getChatElement()
-  if (chatElement.tagName !== 'TEMPLATE') {
-    chatElement.addEventListener('pointerdown', onStartDrag.bind(this, chatElement))
-    dragStartRegistered = true
-    console.log(`[${DISPLAY_NAME}] Drag start listener registered`)
-  } else {
-    console.debug(`[${DISPLAY_NAME}] Skipping to register chat drag handler because the chat isn't rendered yet.`)
-  }
-}
-
-function registerDragResizeListener () {
-  registerDragStartListener()
-  document.addEventListener('pointermove', onDrag)
-  document.addEventListener('pointerup', onStopDrag)
-  document.addEventListener('pointercancel', onStopDrag)
-}
 
 Hooks.on('init', () => {
-  createStyleElement()
-  registerSettings()
+    game.combatChat = new CombatChat()
+    game.combatChat.init()
 })
 
 Hooks.on('ready', () => {
-  injectSidebarHook()
-  registerDragResizeListener()
-  updateCombatTrackerStyle()
-  updateStyleElement()
-})
-
-Hooks.on('renderSidebarTab', function () {
-  registerDragStartListener()
+    game.combatChat.ready()
 })
---
+++ scripts/constants.js
@@ -0,0 +1,20 @@
+export const MODULE = {
+    ID: 'combat-chat',
+    NAME: 'Combat Chat'
+}
+export const DICE_TRAY_MODULE_ID = 'dice-calculator'
+export const COMBAT_CHAT_CLASS = 'combat-chat'
+export const COMBAT_CHAT_DICE_TRAY_CLASS = 'combat-chat-dice-tray'
+export const HIDE_CHAT_INPUT_CLASS = 'hide-chat-input'
+export const CHAT_ID = 'chat'
+export const CHAT_LOG_ID = 'chat-log'
+export const COMBAT_ID = 'combat'
+export const TABS_ID = 'sidebar-tabs'
+export const ACTIVE_CLASS = 'active'
+export const STYLE_ID = `${MODULE.ID}-styles`
+export const SETTING = {
+    RESTYLE_DICE_TRAY: 'restyleDiceTray',
+    CHAT_LOG_DISPLAY: 'showChatOnCombatTrackerTab',
+    CHAT_INPUT_DISPLAY: 'showChatInputInSplitView',
+    CHAT_HEIGHT: 'chatHeight'
+}
